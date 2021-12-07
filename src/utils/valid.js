ca8d3aef12e32aecf3e0fde2546df56ab29843fb
稻草人儿木 <fanliningdida@qq.com>
2021-12-07 16:46:20 +0800

    修改valid

--- src/utils/valid.js
+++ src/utils/valid.js
@@ -12,6 +12,16 @@ export function validLoginName(str){
     return userName(str)||userAccount(str)
 }
 
+export function validPhone(val){
+    const reg = /^(13[0-9]|14[5|7]|15[0|1|2|3|4|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/
+    return reg.test(val)
+}
+
+export function validEmail(val){
+    const reg = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+    return reg.test(val)
+}
+
 export function validPassword(val){
     const reg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,10}$/
     return reg.test(val)
